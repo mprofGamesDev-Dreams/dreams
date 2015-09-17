5ce83a045b97cc9714b265a0f27ebfef558af30a
Glenn Cullen <1100862@MC10794.uad.ac.uk>
2015-09-17 16:46:33 +0100

    UI resource bars

---
+++ Assets/Scripts/UI/ResourceBar.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ResourceBar : MonoBehaviour {
+
+	public RectTransform barTransform;
+	private float cachedY;
+	private float minX;
+	private float maxX;
+	private int currentTotal;
+	[SerializeField] private int maxTotal;
+	[SerializeField] private int startTotal;
+	public Text BarText;
+	public Image visualbar;
+	public GameObject player;
+	private TestRecord playerRecord;
+	// Use this for initialization
+	void Start () {
+		playerRecord = player.GetComponent<TestRecord> ();
+		barTransform = visualbar.rectTransform;
+		cachedY = barTransform.position.y;
+		maxX = barTransform.position.x;
+		minX = barTransform.position.x - (barTransform.rect.width);
+		startTotal = playerRecord.startHealth;
+		currentTotal = startTotal;
+		maxTotal = playerRecord.maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (playerRecord.health != currentTotal) 
+		{
+			currentTotal = playerRecord.health;
+			handleResource();
+		}
+	}
+	private void handleResource()
+	{
+		BarText.text = ""+ currentTotal;
+		float currentXVal = MapValues (currentTotal, 0, maxTotal, minX, maxX);
+
+		barTransform.position = new Vector3 (currentXVal, cachedY);
+
+		if (currentTotal > maxTotal / 2) 
+		{
+			visualbar.color = new Color32((byte)MapValues(currentTotal,maxTotal/2,maxTotal,255,0),255,0,255);
+		} 
+		else 
+		{
+			visualbar.color = new Color32(255,(byte)MapValues(currentTotal,0,maxTotal/2,0,255),0,255);
+		}
+	}
+
+		
+	private float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
+	{
+		return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	}
+}
---
+++ Assets/Scripts/UI/TestRecord.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRecord : MonoBehaviour {
+
+	public int health;
+	public 	int maxHealth;
+	public 	int startHealth;
+	// Use this for initialization
+	void Start () {
+		health = startHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
