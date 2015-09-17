e6b2c63134ec259f30063a3c477738907a144d71
unknown <1505202@MC10792.uad.ac.uk>
2015-09-17 16:42:56 +0100

    Added Inventory System

---
+++ Assets/Scripts/InteractionModule/Behaviour/PickupItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using InventoryModule;
+
+namespace InteractionModule.Behaviours
+{
+	public class PickupItem : A_InteractBehaviour
+	{
+		public override void Initialize (A_InteractBehaviour behaviourParameters)
+		{
+			
+		}
+		
+		public override void OnEventTrigger ()
+		{
+			this.GetComponent<Item>().PickupItem();
+		}
+		
+		public override void OnEventEnd ()
+		{
+			// Do Nothing
+		}
+	}
+}
