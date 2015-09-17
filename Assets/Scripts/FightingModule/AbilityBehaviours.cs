52b2cb9be2a54e33540adbcea8017bc1a958b8c6
unknown <1505202@MC10792.uad.ac.uk>
2015-09-17 13:43:24 +0100

    optimized combat logic -Nelson

---
+++ Assets/Scripts/FightingModule/AbilityBehaviours.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityBehaviours : MonoBehaviour 
+{
+	// Serialize Private Fields To Show Up In The Insepctor
+	[SerializeField] private float attackRange = 100.0f;
+	
+	[SerializeField] private ParticleSystem myParticleSystem;
+
+	private Transform myCameraTransform;
+
+	private void Start () 
+	{
+		// Gets The Main Camera's Transform On Object Startup
+		myCameraTransform = Camera.main.GetComponent<Transform>();
+	}
+	
+	private void Update () 
+	{		
+		if (Input.GetKeyDown(KeyCode.L))//LOGIO
+		{
+			RaycastHit hit;
+			Ray pointer = new Ray(myCameraTransform.position, myCameraTransform.forward * attackRange);
+			
+			Debug.DrawRay(pointer.origin, pointer.direction, Color.black);
+			
+			myParticleSystem.startColor = Color.yellow;
+			myParticleSystem.Emit(1);
+			
+			if (Physics.Raycast(pointer, out hit, attackRange))
+			{
+				Shield shieldScript = hit.transform.GetComponent<Shield>();
+				if(shieldScript != null)
+					shieldScript.ApplyDamage(Shield.shieldOptions.Logio, 10	);
+			}
+		}
+		
+		if (Input.GetKeyDown(KeyCode.M))//IMAGI
+		{
+			RaycastHit hit;
+			Ray pointer = new Ray(myCameraTransform.position, myCameraTransform.forward * attackRange);
+			
+			Debug.DrawRay(pointer.origin, pointer.direction, Color.black);
+			
+			myParticleSystem.startColor = Color.cyan;
+			myParticleSystem.Emit(1);
+			
+			if (Physics.Raycast(pointer, out hit, attackRange))
+			{
+				Shield shieldScript = hit.transform.GetComponent<Shield>();
+				if(shieldScript != null)
+					shieldScript.ApplyDamage(Shield.shieldOptions.Imagi, 10	);
+			}
+		}
+		
+		if (Input.GetKeyDown(KeyCode.V))//VOID
+		{
+			RaycastHit hit;
+			Ray pointer = new Ray(myCameraTransform.position, myCameraTransform.forward * attackRange);
+			
+			Debug.DrawRay(pointer.origin, pointer.direction, Color.black);
+			
+			myParticleSystem.startColor = Color.blue;
+			myParticleSystem.Emit(1);
+			
+			if (Physics.Raycast(pointer, out hit, attackRange))
+			{
+				Shield shieldScript = hit.transform.GetComponent<Shield>();
+				if(shieldScript != null)
+					shieldScript.ApplyDamage(Shield.shieldOptions.Void, 10	);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/FightingModule/PunchingControllerPrototype.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchingControllerPrototype : MonoBehaviour 
+{
+	[SerializeField] private string attackTrigger;
+	[SerializeField] private float attackRange;
+
+	[SerializeField][Range(0,1)] private float attackFromNormalizedTime;
+	[SerializeField][Range(0,1)] private float attackToNormalizedTime;
+
+	[SerializeField] private Animator animationController;
+
+	[SerializeField]private LayerMask raycastTargets;
+
+
+	private int attTrig;
+
+	private bool canAttack = true;
+
+	private Transform cameraTransform;
+
+	private void Start () 
+	{
+		attTrig = Animator.StringToHash(attackTrigger);
+
+		cameraTransform = Camera.main.GetComponent<Transform>();
+
+		if( attackToNormalizedTime < attackFromNormalizedTime ) 
+		{
+			Debug.LogError("attackToNormalizedTime < attackFromNormalizedTime");
+		}
+	}
+	
+	private void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Mouse0) && !animationController.GetCurrentAnimatorStateInfo(0).IsName("AttackTrigger"))
+		{
+			animationController.SetTrigger( attTrig );
+		}
+
+		if(animationController.GetCurrentAnimatorStateInfo(0).IsName("Punch") && canAttack)
+		{
+			if(animationController.GetCurrentAnimatorStateInfo(0).normalizedTime > attackFromNormalizedTime && animationController.GetCurrentAnimatorStateInfo(0).normalizedTime < attackToNormalizedTime )
+				Attack();
+		}
+
+		if(!animationController.GetCurrentAnimatorStateInfo(0).IsName("Punch"))
+		{
+			canAttack = true;
+		}
+	}
+
+	private void Attack()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast( cameraTransform.position, cameraTransform.forward, out hit, attackRange, raycastTargets ))
+		{
+#if UNITY_EDITOR
+			Debug.DrawLine(cameraTransform.position, hit.point, Color.green);
+#endif	 
+			EnemyScript es;
+			if((es = hit.collider.GetComponent<EnemyScript>()) != null)// Is Enemy
+			{
+				canAttack = false;
+				es.TakeSA();
+			}
+		}
+#if UNITY_EDITOR
+		else Debug.DrawRay(cameraTransform.position, cameraTransform.forward * attackRange, Color.red);
+#endif
+	}
+}
