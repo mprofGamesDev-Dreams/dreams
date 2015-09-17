e1cd900b23d30be7da5c88a3aaffd1be8e209e8f
unknown <1100484@MC10801.uad.ac.uk>
2015-09-17 15:50:29 +0100

    Added Keys and Player Stats -Louis

---
+++ Assets/Scripts/DoorsModule/KeyItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Just a class for storing key details
+ */
+
+[System.Serializable]
+public class KeyItem
+{
+	public string Name;
+	public bool Collected;
+
+	// Checks the key name against a passed string
+	public bool CheckName(string Other)
+	{
+		return Name.Equals(Other);
+	}
+	
+	// Return whether key is collected
+	public bool IsCollected()
+	{
+		return Collected;
+	}
+	
+	// Flag the key is collected
+	public void SetCollected()
+	{
+		Collected = true;
+	}
+}
---
+++ Assets/Scripts/DoorsModule/KeyPickup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Script to handle picking up the keys
+ * On trigger collision, it will flag that the key has been collected in the player keychain
+ * 
+ * ATTACHMENT : Attach to the key object in game and set the key name.
+ * Make sure name matches up with a name in the keychain script attached to the player.
+ */
+
+public class KeyPickup : MonoBehaviour
+{
+	public string KeyName = "";
+
+	void Start ()
+	{
+	
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log(col.gameObject.name);
+
+		if( col.gameObject.name == "Player")
+		{
+			// Get access to the key chain
+			KeyChain PlayerKeys = GameObject.Find("Player").GetComponent<KeyChain>();
+
+			// Collect the keys
+			PlayerKeys.CollectKey(KeyName);
+
+			// Destroy this object
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
