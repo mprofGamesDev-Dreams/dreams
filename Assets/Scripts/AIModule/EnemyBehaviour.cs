15cd99312a0bd045f70f75451a6db9c5c380dde7
unknown <1103410@MC10786.uad.ac.uk>
2015-09-18 11:54:03 +0100

    Added AI test scene and AI behaviour script

---
+++ Assets/Scripts/AIModule/EnemyBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float lineOfSightRange = 20.0f;
+    private GameObject targetPlayer;
+    private Vector3 original;
+    private Vector3 target;
+    private NavMeshAgent agent;
+	// Use this for initialization
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        targetPlayer = GameObject.FindGameObjectWithTag("Player");
+        target = transform.position;
+        original = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+        Ray lineOfSight = new Ray(gameObject.transform.position, targetPlayer.transform.position - gameObject.transform.position);
+
+        Debug.DrawRay(lineOfSight.origin,lineOfSight.direction*lineOfSightRange);
+
+       
+
+        if (Physics.Raycast(lineOfSight, out hit, lineOfSightRange))
+        {
+
+            if (hit.transform.gameObject.tag == "Player")
+            {
+                target = targetPlayer.transform.position;
+                agent.SetDestination(target);
+            }
+            else
+            {
+                agent.SetDestination(original);
+            }
+
+           
+        }
+        else
+        {
+            agent.SetDestination(original);
+        }
+
+        
+        
+	}
+}
