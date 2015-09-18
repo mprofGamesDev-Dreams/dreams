d4627229c43fc35b4d9edc03d12804817d09c1da
unknown <1505202@MC10817.uad.ac.uk>
2015-09-18 12:57:32 +0100

    Bug-fixed Inventory System

---
+++ Assets/Scripts/InventoryModule/Abstract/A_ItemBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace InventoryModule
+{
+	public abstract class A_ItemBehaviour : MonoBehaviour 
+	{
+		// Parent Reference to be able to access it in case you need to destroy or drop after use
+		[SerializeField] public Item parentReference;
+
+		// Abstract classes to act and initialize
+		public abstract void Initialize( Item parent,  A_ItemBehaviour behaviourParameters );
+		public abstract void ItemAction();
+	}
+}	
