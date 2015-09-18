778a457fea32b2ccc297b46a30e4de8c31468cb6
BenDoherty557 <1103410@MC10795.uad.ac.uk>
2015-09-18 15:59:47 +0100

    Added animation to shield script

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -8,12 +8,24 @@ public class Shield : MonoBehaviour
 	[SerializeField] private shieldOptions shieldType;
     [SerializeField] private int shieldHealth = 20;
 
+    private Renderer tempRenderer;
+    private float scrollOffset = 0;
+    private float scrollSpeed = 0.003f;
+
+    private void Start()
+    {
+        tempRenderer = GetComponent<Renderer>();
+    }
+
 	private void Update () 
 	{
         if (shieldHealth <= 0)
         {
 			Destroy(gameObject);
         }
+
+        scrollOffset += scrollSpeed;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(scrollOffset / 2, scrollOffset));
 	}
 
     public void ApplyDamage( shieldOptions damageType, int damageIn)
