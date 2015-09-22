07a979db4759b064a20644f9261fe8eab6c3d77c
Callum Kenny <1502000@abertay.ac.uk>
2015-09-22 16:10:54 +0100

    Added teleportation and white screen fade

---
+++ Assets/Scripts/Teleporting/TeleporterSender.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleporterSender : MonoBehaviour
+{
+	
+	[SerializeField] private GameObject player;
+	[SerializeField] private GameObject sender;
+	[SerializeField] private GameObject receiver;
+	[SerializeField] private WhiteFlash flash;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter ()
+	{
+		flash.RequestFlash ();
+		StartCoroutine (Teleport ());
+	}
+
+	IEnumerator Teleport(){
+		//Delay teleporting to allow screen to fade to white
+		yield return new WaitForSeconds (1.0f);
+		// Move player
+		player.transform.position = receiver.transform.position;
+		// Destroy teleport objects as no longer required
+		Destroy (sender);
+		Destroy (this);
+		Destroy (receiver);
+	}
+}
---
+++ Assets/Scripts/Teleporting/WhiteFlash.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WhiteFlash : MonoBehaviour
+{
+	[SerializeField] private Image image;
+	private float fadeInSpeed = 10f;         // Speed that the screen fades to white
+	private float fadeOutSpeed = 2.0f;			// Speed that the screen fades to clear
+	private bool flashRequested = false;
+	private bool toWhite;
+	private float delay;
+	
+	void Awake ()
+	{
+
+	}
+	
+	void Update ()
+	{
+		if (flashRequested) {
+			// Check if fading to or from white
+			if (toWhite) {
+				// Continue fading until image is almost white
+				if (image.color.a < 0.995) {
+					FadeToWhite ();
+				} 
+				// Once image is almost white, set to pure white and delay slightly
+				else {
+					image.color = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+					toWhite = false;
+					delay = Time.time + 0.5f;
+				}
+			} else if (Time.time > delay) {
+				// After brief delay, fade white to clear
+				if (image.color.a > 0.005) {
+					FadeToClear ();
+				}
+				// When image is almost fully clear, disable it
+				else{
+					image.enabled = false;
+					flashRequested = false;
+				}
+			}
+		}
+	}
+
+	// Begins the flash process
+	public void RequestFlash ()
+	{
+		flashRequested = true;
+		image.enabled = true;
+		toWhite = true;
+	}
+	
+	public void FadeToClear ()
+	{
+		// Lerp the colour of the panel between itself and transparent
+		image.color = Color.Lerp (image.color, Color.clear, fadeOutSpeed * Time.deltaTime);
+	}
+	
+	public void FadeToWhite ()
+	{
+		// Lerp the colour of the panel between itself and white
+		image.color = Color.Lerp (image.color, Color.white, fadeInSpeed * Time.deltaTime);
+	}
+}
