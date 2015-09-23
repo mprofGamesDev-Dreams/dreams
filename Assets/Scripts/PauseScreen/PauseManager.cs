638795cab4a27012d9c4a1fdae72e43d01cfc33d
Callum Kenny <1502000@abertay.ac.uk>
2015-09-23 14:10:39 +0100

    pause screen

---
+++ Assets/Scripts/PauseScreen/PauseManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseManager : MonoBehaviour 
+{
+	
+	public bool is_paused;
+	
+	// Pause pop-up
+	[SerializeField] private GameObject pause_screen;
+	[SerializeField] private GameObject pause_menu;
+	[SerializeField] private GameObject HUD;
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		// Initialise canvases to correspond to a main menu greeting the player
+		is_paused = false;
+		
+		// Pause screen and its menus
+		SetInitialPauseMenu ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Reserve "Esc" key for pausing game
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			is_paused = !is_paused;
+			if(is_paused) 
+			{
+				PauseGame();
+			}
+			else
+			{
+				ResumeGame();
+				SetInitialPauseMenu();
+			}
+		}
+	}
+	
+	private void SetInitialPauseMenu()
+	{
+		pause_screen.SetActive (false);
+		pause_menu.SetActive (true);
+	}
+	
+	public void PauseGame()
+	{
+		is_paused = true;
+		pause_screen.SetActive (is_paused);
+		HUD.SetActive (!is_paused);
+		Time.timeScale = 0.0f;
+	}
+	
+	public void ResumeGame()
+	{
+		is_paused = false;
+		pause_screen.SetActive (is_paused);
+		HUD.SetActive (!is_paused);
+		Time.timeScale = 1.0f;
+	}
+
+	public void RestartGame()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+		ResumeGame ();
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+	
+	public void BackToPauseMenu(GameObject to_hide)
+	{
+		to_hide.SetActive (false);
+		pause_menu.SetActive (true);
+	}
+	
+	public void OpenFromPause(GameObject to_show)
+	{
+		to_show.SetActive (true);
+		pause_menu.SetActive (false);
+	}
+	
+}
