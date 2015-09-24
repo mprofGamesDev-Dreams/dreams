6c52a0603568db5ffa760d7af0969db34b800cdb
BenDoherty557 <1103410@MC10795.uad.ac.uk>
2015-09-24 10:29:32 +0100

    modified modelSwitch script to be triggered by a collision box. Also imported 3rd Level concept

---
+++ Assets/Scripts/Transformable/ModelSwitchTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*ModelSwitchTrigger
+ * 
+ * This Script should be attached to a gameobject with a trigger collider
+ * And the the game object to be added will dissapear and spawn an object in its place when this object is entered
+ */
+
+public class ModelSwitchTrigger : MonoBehaviour {
+
+    [SerializeField] private GameObject transformableObject;
+	
+    void OnTriggerEnter(Collider colliderIn)
+    {
+        transformableObject.GetComponent<ModelSwitch>().SendMessage("Trigger");
+        Destroy(gameObject);
+    }
+}
