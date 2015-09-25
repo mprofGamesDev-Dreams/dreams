4adf9179880db65c1952d532ef7881cc61b58c28
Nelson <1505202@MC10790.uad.ac.uk>
2015-09-25 16:18:54 +0100

    Added Enemy Characteristics

---
+++ Assets/Scripts/AIModule/Characteristics/EnemyScale.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScale : MonoBehaviour 
+{
+	[SerializeField] private Vector3 targetScale;
+	[SerializeField] private float scaleTime;
+	private bool canGrow = true;
+	private Transform myTransform;
+	
+	
+	private void Start () 
+	{
+		myTransform = GetComponent<Transform>();
+	}
+	
+	private void Update()
+	{
+		if (canGrow) 
+		{
+			myTransform.localScale = Vector3.Lerp( myTransform.localScale, targetScale, Time.deltaTime * scaleTime );
+			if(myTransform.localScale == targetScale)
+			{
+				canGrow = !canGrow;
+			}
+		}
+	}
+}
