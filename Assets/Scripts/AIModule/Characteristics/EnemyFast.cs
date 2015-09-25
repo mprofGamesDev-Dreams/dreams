72fc8d9edcc9578abe874478463ca5c2b1840840
Nelson <1505202@MC10790.uad.ac.uk>
2015-09-25 16:28:57 +0100

    Added Enemy Characteristics

---
+++ Assets/Scripts/AIModule/Characteristics/EnemyFast.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFast : MonoBehaviour 
+{
+	private NavMeshAgent navMeshAgent;
+	private EnemyBehaviour behaviour;
+	private EnemyScript stats;
+
+	[SerializeField] private float speedMultiplier = 2;
+	[SerializeField] private float damageMultiplier = 2;
+	[SerializeField] [Range(0, 1)] private float healthMultiplier = 0.5f;
+
+	private void Start()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		behaviour = GetComponent<EnemyBehaviour>();
+		stats = GetComponent<EnemyScript>();
+
+		navMeshAgent.speed *= speedMultiplier;
+		behaviour.attackDamage *= damageMultiplier;
+		stats.MaxHealth *= healthMultiplier;
+	}
+}
