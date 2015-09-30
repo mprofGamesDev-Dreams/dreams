333fad2dcd6d4a93bf0aadbe7333c7de9e4a8f66
Louis <1100484@live.abertay.ac.uk>
2015-09-30 15:34:49 +0100

    Added scripts for destroying and resetting an object when the player dies.

---
+++ Assets/Scripts/DestroyOnPlayerDeath.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Script for destroying a gameobjects position on player death
+ * 
+ * ATTACHMENT: Add to a gameobject prefab
+ * 
+ */
+
+public class DestroyOnPlayerDeath : MonoBehaviour
+{
+	private GameObject Player;
+	private PlayerStats Stats;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		// Find the player
+		Player = GameObject.Find ("Player");
+		
+		// Get access to stats
+		Stats = Player.GetComponent<PlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// If the player is dead
+		if(Stats.IsDead)
+		{
+			// Destroy self
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ResetOnPlayerDeath.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Script for resetting a gameobjects position on player death
+ * 
+ * ATTACHMENT: Add to a gameobject prefab
+ * 
+ */
+
+public class ResetOnPlayerDeath : MonoBehaviour
+{
+	private GameObject Player;
+	private PlayerStats Stats;
+	private Vector3 StartingPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// Find the player
+		Player = GameObject.Find ("Player");
+
+		// Get access to stats
+		Stats = Player.GetComponent<PlayerStats>();
+
+		// Store the initial state
+		StartingPosition = this.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// If the player is dead
+		if(Stats.IsDead)
+		{
+			// Reset the gameobject
+			this.gameObject.transform.position = StartingPosition;
+		}
+	}
+}
