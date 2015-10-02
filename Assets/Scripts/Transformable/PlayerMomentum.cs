f1c3da48f2c442a099cd7a64eb75bbef518bd397
Glenn Cullen <1100862@MC10794.uad.ac.uk>
2015-10-02 16:00:41 +0100

    bug fix

--- Assets/Scripts/Transformable/PlayerMomentum.cs
+++ Assets/Scripts/Transformable/PlayerMomentum.cs
@@ -5,9 +5,10 @@ using System.Collections;
 // must be added to a trigger that is a child of the player object.
 public class PlayerMomentum : MonoBehaviour {
 
+	private Vector3 playerScale;
 	// Use this for initialization
 	void Start () {
-	
+		playerScale = gameObject.transform.parent.localScale;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +24,9 @@ public class PlayerMomentum : MonoBehaviour {
 		{
             //Debug.Log("collided with " + collider.name);		
 			gameObject.transform.parent.parent = collider.transform;
+			gameObject.transform.parent.localScale = new Vector3(playerScale.x/collider.transform.localScale.x,
+			                                                     playerScale.y/collider.transform.localScale.y,
+			                                                     playerScale.z/collider.transform.localScale.z);
 		}
 	}
 
