b519190e685b7710a2052b1b34a1343388943b67
Glenn Cullen <1100862@MC10794.uad.ac.uk>
2015-10-09 12:48:02 +0100

    added textured teleporter

---
+++ Assets/Scripts/TriggerSky.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSky : MonoBehaviour {
+
+	public bool isTriggered = false;
+	public GameObject image;
+	[SerializeField] private float fadeInTime = 2.0f;
+	[SerializeField] private float fadeInTimer = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isTriggered) {
+			StartCoroutine("Fade");
+		}
+	}
+
+	IEnumerator Fade()
+	{
+		isTriggered = false;
+		Renderer SkyTexture = gameObject.GetComponent<Renderer> ();
+		Color targetColor = SkyTexture.material.color;
+		targetColor.a = 0.0f;
+		//SkyTexture.SetColor(targetColor);
+		SkyTexture.material.color = targetColor;
+		while (fadeInTimer < fadeInTime) {
+
+			fadeInTimer += Time.deltaTime;
+			targetColor.a = fadeInTimer/1;
+			SkyTexture.material.color = targetColor;
+			yield return null;
+		}
+	}
+}
